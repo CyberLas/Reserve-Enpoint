67ffc2bbe043727ae7a9cc83ee8a8922c4b790c3
Carlos Angeles <caamnil@gmail.com>
2022-09-23 13:08:29 -0500

    Corrigiendo constantes

--- app/constant/index.js
+++ app/constant/index.js
@@ -1,15 +1,15 @@
-import 'dotenv/config'
+import "dotenv/config";
 
 // DATA SERVER
-export const PORT = process.env.PORT
-export const URL = process.env.URL
+export const PORT = process.env.PORT;
+export const URL = process.env.URL;
 
 // CREDENTIALS POSTGRESQL DATABASE
-export const POSTGRESHOST = process.env.POSTGRESHOST
-export const POSTGRESPORT = process.env.POSTGRESPORT
-export const POSTGRESNAME = process.env.POSTGRESNAME
-export const POSTGRESUSER = process.env.POSTGRESUSER
-export const POSTGRESPASS = process.env.POSTGRESPASS
+export const POSTGRESHOST = process.env.POSTGRESHOST;
+export const POSTGRESPORT = process.env.POSTGRESPORT;
+export const POSTGRESNAME = process.env.POSTGRESNAME;
+export const POSTGRESUSER = process.env.POSTGRESUSER;
+export const POSTGRESPASS = process.env.POSTGRESPASS;
 
 // DEVELOPMENT
-export const DEV = JSON.parse(process.env.DEV)
+export const DEV = JSON.parse(process.env.DEV);
